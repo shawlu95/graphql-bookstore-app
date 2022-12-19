765a05b5d9543e21bd7865c8184dc12b3865467a
Shaw Lu <shawlu95@gmail.com>
2022-12-19 10:39:21 -0800

    use lodash, sample data

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+var books = [
+  { name: 'The House of Morgan', genre: 'History', id: '1' },
+  { name: 'Gone with the Wind', genre: 'Fiction', id: '2' },
+  { name: 'Zero to One', genre: 'Nonfiction', id: '3' },
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -19,6 +26,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // get data from db, using the args
+        return _.find(books, { id: args.id });
       },
     },
   },
