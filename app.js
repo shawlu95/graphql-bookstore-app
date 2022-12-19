5c6ece46849e542c9ceb8717d8f3dd4078bd0b99
Shaw Lu <shawlu95@gmail.com>
2022-12-19 10:20:11 -0800

    define schema

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-
+const schema = require('./schema/schema');
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+/**
+ * Required params:
+ * 1. a schema of the graph data
+ */
+app.use('/graphql', graphqlHTTP({ schema }));
 
 app.listen(4000, () => {
   console.log('now listening on port 4000');
